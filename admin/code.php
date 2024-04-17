889ea3c9d2c0cca81ea8f462367896dbfe13bddb
Git29sakshi <143997416+Git29sakshi@users.noreply.github.com>
2024-04-17 13:12:47 +0530

    commit

---
+++ admin/code.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+$connection = mysqli_connect("localhost","root","","adminpanel");
+
+if(isset($_POST['registerbtn']))
+{
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['confirmpassword'];
+
+    if($password === $cpassword)
+    {
+        $query = "INSERT INTO register (username,email,password ) VALUES ('$username','$email','$password')";
+        $query_run = mysqli_query($connection, $query);
+
+        if($query_run)
+        {
+            //echo "saved";
+            $_SESSION['success'] = "Admin Profile Added";
+            header('Location: register.php');
+        }
+        else
+        {
+            $_SESSION['status'] = "Admin Profile Not Added";
+            header('Location: register.php');
+        }
+    }
+    else
+    {
+        $_SESSION['status'] = "Password and Confirm Password Does Not Match";
+        header('Location: register.php');
+    }
+}
+
+
+
+if(isset($_POST['updatebtn']))
+{
+    $id = $_POST['edit_id'];
+    $username = $_POST['edit_username'];
+    $email = $_POST['edit_email'];
+    $password = $_POST['edit_password'];
+
+    $query = "UPDATE register SET username='$username', email='$email', password='$password' WHERE id='$id' ";
+    $query_run = mysqli_query($connection , $query);
+
+    if($query_run)
+    {
+        $_SESSION['success'] = "Your Data is Uploaded";
+        header('Location: register.php');
+    }
+    else
+    {
+        $_SESSION['status'] = "Your Data is Not Uploaded";
+        header('Location: register.php');
+    }
+}
+
+
+
+
+if(isset($_POST['delete_btn']));
+{
+    $id = $_POST['delete_id'];
+
+    $query = "DELETE FROM register WHERE id='$id'";
+    $query_run = mysqli_query($connection , $query);
+
+    if($query_run)
+    {
+        $_SESSION['success'] = "Your Data is Deleted";
+        header('Location: register.php');
+    }
+    else
+    {
+        $_SESSION['status'] = "Your Data is Not Deleted";
+        header('Location: register.php');
+    }
+}
+
+
+
+?>
